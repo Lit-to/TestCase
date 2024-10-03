f20c80ac2a8b8e265f409303aad594cb6e146c5a
Lit-to <swordworld0327@gmail.com>
2024-10-03 18:29:19 +0900

    デバッグ出力を一時的に追加

--- main.py
+++ main.py
@@ -81,6 +81,7 @@ class testCaseModal(discord.ui.Modal):
         icon={"abc":":blue_circle:","arc":":green_circle:","agc":":orange_circle:"}
         await interaction.followup.send("## "+icon[query[0]]+file_name[1].upper()+" :regional_indicator_"+file_name[2].lower()+":"+" の入力ファイル("+file_name[4]+")を送信中...:")
         await interaction.followup.send(file=case_file[0])
+        print(file_name)
         await interaction.followup.send("## "+icon[query[0]]+file_name[1].upper()+" :regional_indicator_"+file_name[2].lower()+":"+" の出力ファイル("+file_name[4]+")を送信中...:")
         await interaction.followup.send(file=case_file[1])
 
